54cdc6e7733ea219f742da2ffd5f745e8271353b
Daniel <dtkahn@gmail.com>
2018-08-29 20:58:45 -0400

    installed axios, have test query setting state with an array from axios search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      // Result of the actor search
+      searchResults: [],
+
+      // Information on actor1
+      actor1: {},
+
+      // Information on actor2
+      actor2: {},
+
+      // Initually contain all of the films from actor 1 and actor 2
+      //// Then will be filtered for movies which have both actor1 and actor2
+      movies: []
+
+    }
+  }
+  
+  componentDidMount(){
+
+    Axios({
+      url: `https://api.themoviedb.org/3/search/person`,
+      params: {
+        api_key: `972b4433f3e8f302aee3055dd209330c`,
+        query: 'Robert Elliott'
+      }
+    })
+      .then((res) => {
+        console.log('res: ',res)
+        this.setState({
+          searchResults: res.data.results
+        });
+      });
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie Match</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
